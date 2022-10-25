01b9c88062b1803fd5acbcde0928b9736c39c31f
Frode Austvik <edorfaus@xepher.net>
2022-10-25 21:36:57 +0000

    Refactor the tests, to make it easier to add more

--- ed2k_test.go
+++ ed2k_test.go
@@ -8,79 +8,71 @@ func TestZeroOneChunk(t *testing.T) {
 	zf := make([]byte, 9728000)
 	h := New()
 	h.setTest(t)
-	n, err := h.Write(zf)
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	if n != 9728000 {
-		t.Fatalf("Bad write size: %d", n)
-	}
+	verifyWrite(t, h, 1, zf)
 
-	blue, err := h.SumBlue()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	red, err := h.SumRed()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if blue != "d7def262a127cd79096a108e7a9fc138" {
-		t.Logf("blue failed: %s", blue)
-		t.Fail()
-	}
-
-	if red != "fc21d9af828f92a8df64beac3357425d" {
-		t.Logf("red failed: %s", red)
-		t.Fail()
-	}
+	verifyBlueRed(
+		t, h,
+		"d7def262a127cd79096a108e7a9fc138",
+		"fc21d9af828f92a8df64beac3357425d",
+	)
 }
 
 func TestZeroTwoChunk(t *testing.T) {
 	zf := make([]byte, 9728000)
 	h := New()
 	h.setTest(t)
-	n, err := h.Write(zf)
+
+	verifyWrite(t, h, 1, zf)
+	verifyWrite(t, h, 2, zf)
+
+	verifyBlueRed(
+		t, h,
+		"194ee9e4fa79b2ee9f8829284c466051",
+		"114b21c63a74b6ca922291a11177dd5c",
+	)
+}
+
+func verifyWrite(t *testing.T, h *Ed2k, i int, b []byte) {
+	t.Helper()
+
+	n, err := h.Write(b)
 	if err != nil {
-		t.Fatal(err)
+		t.Logf("Write %v failed: %v", i, err)
+		if n != len(b) {
+			t.Logf("Bad size: expected %v, got %v", len(b), n)
+		}
+		t.FailNow()
+	}
+	if n != len(b) {
+		t.Fatalf("Write %v: bad size: expected %d, got %d", i, len(b), n)
+	}
+}
+
+func verifyBlueRed(t *testing.T, h *Ed2k, expectBlue, expectRed string) {
+	t.Helper()
+
+	gotBlue, errBlue := h.SumBlue()
+	gotRed, errRed := h.SumRed()
+
+	if errBlue != nil {
+		t.Errorf("SumBlue error: %v", errBlue)
+	}
+	if errRed != nil {
+		t.Errorf("SumRed error: %v", errRed)
 	}
 
-	if n != 9728000 {
-		t.Fatalf("Bad 1st write size: %d", n)
-	}
-
-	n, err = h.Write(zf)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if n != 9728000 {
-		t.Fatalf("Bad 2nd write size: %d", n)
-	}
-
-	blue, err := h.SumBlue()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	red, err := h.SumRed()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if blue != "194ee9e4fa79b2ee9f8829284c466051" {
-		t.Logf("blue failed: %s", blue)
-		t.Fail()
+	if gotBlue != expectBlue {
+		t.Errorf("blue failed: %s", gotBlue)
+		t.Errorf("   expected: %s", expectBlue)
 	} else {
-		t.Logf("blue passed: %s", blue)
+		t.Logf("blue passed: %s", gotBlue)
 	}
 
-	if red != "114b21c63a74b6ca922291a11177dd5c" {
-		t.Logf("red failed: %s", red)
-		t.Fail()
+	if gotRed != expectRed {
+		t.Errorf("red  failed: %s", gotRed)
+		t.Errorf("   expected: %s", expectRed)
 	} else {
-		t.Logf("red passed: %s", red)
+		t.Logf("red  passed: %s", gotRed)
 	}
 }
