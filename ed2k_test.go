3cf88c6f78edd116252271749b4c6b3b2d7e7e94
Frode Austvik <edorfaus@xepher.net>
2022-10-26 00:06:26 +0000

    Add more tests, that all pass

--- ed2k_test.go
+++ ed2k_test.go
@@ -4,33 +4,170 @@ import (
 	"testing"
 )
 
+const chunkSize = 9728000
+
+// Expected hashes for one complete chunk of zeroes
+const zeroOneChunkBlue = "d7def262a127cd79096a108e7a9fc138"
+const zeroOneChunkRed = "fc21d9af828f92a8df64beac3357425d"
+
+// Expected hashes for two complete chunks of zeroes
+const zeroTwoChunkBlue = "194ee9e4fa79b2ee9f8829284c466051"
+const zeroTwoChunkRed = "114b21c63a74b6ca922291a11177dd5c"
+
 func TestZeroOneChunk(t *testing.T) {
-	zf := make([]byte, 9728000)
 	h := New()
 	h.setTest(t)
+
+	zf := make([]byte, chunkSize)
 
 	verifyWrite(t, h, 1, zf)
 
-	verifyBlueRed(
-		t, h,
-		"d7def262a127cd79096a108e7a9fc138",
-		"fc21d9af828f92a8df64beac3357425d",
-	)
+	verifyBlueRed(t, h, zeroOneChunkBlue, zeroOneChunkRed)
 }
 
-func TestZeroTwoChunk(t *testing.T) {
-	zf := make([]byte, 9728000)
+func TestZeroOneChunkTwoWrites(t *testing.T) {
 	h := New()
 	h.setTest(t)
+
+	zf := make([]byte, chunkSize/2)
 
 	verifyWrite(t, h, 1, zf)
 	verifyWrite(t, h, 2, zf)
 
-	verifyBlueRed(
-		t, h,
-		"194ee9e4fa79b2ee9f8829284c466051",
-		"114b21c63a74b6ca922291a11177dd5c",
-	)
+	verifyBlueRed(t, h, zeroOneChunkBlue, zeroOneChunkRed)
+}
+
+func TestZeroTwoChunk(t *testing.T) {
+	h := New()
+	h.setTest(t)
+
+	zf := make([]byte, chunkSize*2)
+
+	verifyWrite(t, h, 1, zf)
+
+	verifyBlueRed(t, h, zeroTwoChunkBlue, zeroTwoChunkRed)
+}
+
+func TestZeroTwoChunkTwoWrites(t *testing.T) {
+	h := New()
+	h.setTest(t)
+
+	zf := make([]byte, chunkSize)
+
+	verifyWrite(t, h, 1, zf)
+	verifyWrite(t, h, 2, zf)
+
+	verifyBlueRed(t, h, zeroTwoChunkBlue, zeroTwoChunkRed)
+}
+
+func TestZeroTwoChunkThreeWrites(t *testing.T) {
+	h := New()
+	h.setTest(t)
+
+	zf := make([]byte, chunkSize)
+
+	// This tests writes that cross the chunk boundary, without being
+	// larger than a chunk.
+	verifyWrite(t, h, 1, zf[:chunkSize/2])
+	verifyWrite(t, h, 2, zf)
+	verifyWrite(t, h, 3, zf[:chunkSize/2])
+
+	verifyBlueRed(t, h, zeroTwoChunkBlue, zeroTwoChunkRed)
+}
+
+func TestZeroOverOneChunk(t *testing.T) {
+	h := New()
+	h.setTest(t)
+
+	zf := make([]byte, chunkSize)
+
+	verifyWrite(t, h, 1, zf[:1])
+	verifyWrite(t, h, 2, zf)
+
+	const targetHash = "06329e9dba1373512c06386fe29e3c65"
+	verifyBlueRed(t, h, targetHash, targetHash)
+}
+
+func TestZeroOneAndHalfChunk(t *testing.T) {
+	h := New()
+	h.setTest(t)
+
+	zf := make([]byte, chunkSize)
+
+	verifyWrite(t, h, 1, zf)
+	verifyWrite(t, h, 2, zf[:chunkSize/2])
+
+	const targetHash = "7dc39579c6e15343361a37d3d4e5b9d2"
+	verifyBlueRed(t, h, targetHash, targetHash)
+}
+
+func TestZeroOverTwoChunks(t *testing.T) {
+	h := New()
+	h.setTest(t)
+
+	zf := make([]byte, chunkSize)
+
+	verifyWrite(t, h, 1, zf)
+	verifyWrite(t, h, 2, zf[:1])
+	verifyWrite(t, h, 3, zf)
+
+	const targetHash = "e57f824d28f69fe90864e17673668457"
+	verifyBlueRed(t, h, targetHash, targetHash)
+}
+
+func TestSizes(t *testing.T) {
+	h := New()
+	s, bs := h.Size(), h.BlockSize()
+	if s != 16 {
+		t.Errorf("Bad Size(): expected %v, got %v", 16, s)
+	}
+	if bs != chunkSize {
+		t.Errorf("Bad BlockSize(): expected %v, got %v", chunkSize, bs)
+	}
+}
+
+func TestReset(t *testing.T) {
+	h := New()
+
+	// For this test, we don't care whether the sums have the expected
+	// values, only whether they're consistent - so don't verify them,
+	// just grab them to compare against the values after the reset.
+
+	getBlueRed := func() (blue, red string) {
+		t.Helper()
+		var err error
+		if blue, err = h.SumBlue(); err != nil {
+			t.Fatalf("SumBlue failed: %v", err)
+		}
+		if red, err = h.SumRed(); err != nil {
+			t.Fatalf("SumRed failed: %v", err)
+		}
+		return blue, red
+	}
+
+	emptyBlue, emptyRed := getBlueRed()
+
+	// Reset should not affect an empty hash
+	h.Reset()
+	verifyBlueRed(t, h, emptyBlue, emptyRed)
+
+	zf := make([]byte, chunkSize)
+
+	verifyWrite(t, h, 1, zf)
+
+	otherBlue, otherRed := getBlueRed()
+
+	if otherBlue == emptyBlue || otherRed == emptyRed {
+		t.Logf("Empty blue/red: %s / %s", emptyBlue, emptyRed)
+		t.Logf("Other blue/red: %s / %s", otherBlue, otherRed)
+		t.Fatalf("Write did not change the sums, so cannot test Reset")
+	}
+
+	h.Reset()
+	verifyBlueRed(t, h, emptyBlue, emptyRed)
+
+	verifyWrite(t, h, 2, zf)
+	verifyBlueRed(t, h, otherBlue, otherRed)
 }
 
 func verifyWrite(t *testing.T, h *Ed2k, i int, b []byte) {
@@ -63,16 +200,16 @@ func verifyBlueRed(t *testing.T, h *Ed2k, expectBlue, expectRed string) {
 	}
 
 	if gotBlue != expectBlue {
-		t.Errorf("blue failed: %s", gotBlue)
-		t.Errorf("   expected: %s", expectBlue)
+		t.Errorf("blue failed: %q", gotBlue)
+		t.Errorf("   expected: %q", expectBlue)
 	} else {
-		t.Logf("blue passed: %s", gotBlue)
+		t.Logf("blue passed: %q", gotBlue)
 	}
 
 	if gotRed != expectRed {
-		t.Errorf("red  failed: %s", gotRed)
-		t.Errorf("   expected: %s", expectRed)
+		t.Errorf("red  failed: %q", gotRed)
+		t.Errorf("   expected: %q", expectRed)
 	} else {
-		t.Logf("red  passed: %s", gotRed)
+		t.Logf("red  passed: %q", gotRed)
 	}
 }
